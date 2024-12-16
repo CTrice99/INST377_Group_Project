1d1c4f7917df6e293f8ba595c456e63ee502714a
shristigg <165525105+shristigg@users.noreply.github.com>
2024-12-15 22:34:23 -0500

    make changes for index

--- docs/Developer-Manual.md
+++
@@ -1,7 +0,0 @@
-How to install our application and all dependencies:
-
-How to run our application on a server:
-
-How to run any tests:
-
-API endpoints:
--- public/About_Us_JS.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('search-button').addEventListener('click', () => {
-    const breedName = document.getElementById('breed-input').value.toLowerCase();
-    if (breedName) {
-      window.location.href = `Results.html?breed=${breedName}`; 
-    }
-  });
