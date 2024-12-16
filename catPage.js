7a178b2b5924bf5ab4190ade868f6066c84cf042
lexmd28 <lexmd28@gmail.com>
2024-12-15 23:38:40 -0500

    final cat page

--- catPage.js
+++ catPage.js
@@ -13,36 +13,25 @@ async function populateCatPage() {
                 var catBreed = result[number]
                 var catBreedDescription = (catBreed[1].description)
                 var imageID = catBreed[1].reference_image_id
-                arr.push(imageID)
+                console.log(catBreedDescription)
                 document.getElementById("catBody").appendChild(div)
-                
-                
-                const api = fetch(`https://api.thecatapi.com/v1/images/${imageID}`)
-                .then((res) => res.json())
-                .then((res) => {
-                    var result = Object.entries(res)
-                    result = (result[1][1])
-                    console.log(result)
-                    return result
-                })
-                var image = document.createElement("img")
-                image.id = "image" + i
-                image.src = api
                 var breedInfo = document.createElement("h3")
-                div.appendChild(breedInfo)
-                div.appendChild(image)
                 breedInfo.id = "breedInfo" + i
                 breedInfo.innerHTML = catBreedDescription
+                console.log(imageID)
+                var image = document.createElement("img")
+                div.appendChild(breedInfo)
+
 
             }
 
 
 
         })
-
-    }
-
+}
 
 
 
-window.onload = populateCatPage()
+
+
+    window.onload = populateCatPage()
