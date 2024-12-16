9542a3abccc4e5ec2e560eac038f2737fe3f802d
Christina Trice <ctrice@terpmail.umd.edu>
2024-12-15 20:42:42 -0500

    Update index.js

--- supabase-client/index.js
+++ supabase-client/index.js
@@ -6,61 +6,59 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 const supabaseUrl = 'https://ebixebwcsjtrkypluumw.supabase.co';
 const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViaXhlYndjc2p0cmt5cGx1dW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyOTg0NTAsImV4cCI6MjA0OTg3NDQ1MH0.JdMnylSVNBgXeLbfWTaajSShNe8fWIvzgxaCsapv9O4';
-const supabase = supabaseClient.createClient(supabaseUrl,supabaseKey);
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViaXhlYndjc2p0cmt5cGx1dW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyOTg0NTAsImV4cCI6MjA0OTg3NDQ1MH0.JdMnylSVNBgXeLbfWTaajSShNe8fWIvzgxaCsapv9O4';
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 
-
-app.get('/clients', async(req, res) => {
-    console.log('Attempting to get all clients.');
-
-    const {data, error} = await supabase.from('client').select();
-
-    if (error){
-        console.log('Error:', error);
-        res.send(error);
-    }else{
-        console.log('Successfully Retreived Data');
-        res.send(data);
-
-
-    }
-    
-    
-
-    res.send('Blah');
+app.get('/', (req, res) => {
+  res.sendFile('Contact.html', { root: __dirname });
 });
 
-app.post('/client', async (req, res) =>{
-    console.log('Attempting to add Client.');
-    console.log('Request',req)
+app.get('/customers', async (req, res) => {
+  console.log('Attempting to get all customers.');
 
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const email = req.body.email;
+  const { data, error } = await supabase.from('customer').select();
+
+  if (error) {
+    console.log('Error:', error);
+    res.send(error);
+  } else {
+    console.log('Successfully Retrieved Data');
+    res.send(data);
+  }
+});
+
+app.post('/customer', async (req, res) => {
+  console.log('Attempting to add Customer.');
+  console.log('Request', req.body);
+
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
 
 
-    const{data,error} = await supabase
-    .from('client')
-    .insert({'client_first_name':firstName, 'client_last_name':lastName,'client_email':emai })
+  const { data, error } = await supabase
+    .from('customer')
+    .insert({
+      customer_first_name: firstName,
+      customer_last_name: lastName,
+      customer_email: email,
+    })
     .select();
-    
-    if (error){
-        console.log('Error:', error);
-        res.send(error);
-    }else{
-        console.log('Successfully Retreived Data');
-        res.send(data);
 
-
-    }
+  if (error) {
+    console.log('Error:', error);
+    res.send(error);
+  } else {
+    console.log('Successfully Retrieved Data');
+    res.send(data);
+  }
 });
 
 app.listen(port, () => {
-    console.log('App is barking');
+  console.log('App is ALIVEEEEEE');
 });
-
